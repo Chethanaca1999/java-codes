c3f57b9f63c73e93c443f8ebe0bb88be09f0bd80
Chethanaca1999 <chethanaca.xworkz@gmail.com>
2021-09-28 21:55:26 +0530

    basic

---
+++ Pen.java
@@ -0,0 +1,13 @@
+class Pen
+{
+public String name;
+public int penId;
+public int price;
+public String colour;
+public String type;
+
+public void write()
+{
+System.out.println("Handhi Story");
+}
+}
