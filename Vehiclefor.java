f4b7f221d69e813063010baa14f71cf33606677b
Chethanaca1999 <chethanaca.xworkz@gmail.com>
2021-09-28 22:09:18 +0530

    forLoop

---
+++ Vehiclefor.java
@@ -0,0 +1,16 @@
+class Vehicle
+{
+static String[]  typesofCars={"electric car","fuelizedcar"};
+static String[] nameoftheCars={"tata","nexa","lamborgini","audi","ferrari","benz","ford"};
+public static void main( String a[])
+{
+for(int i=0;i<typesofCars.length;i++)
+{
+System.out.println(typesofCars[i]);
+}
+for(int i=0;i<nameoftheCars.length;i++)
+{
+System.out.println(nameoftheCars[i]);
+}
+}
+}
