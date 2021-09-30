61c052a5649d90d5491e0778f91f2cc12062cbfd
Chethanaca1999 <chethanaca.xworkz@gmail.com>
2021-09-30 21:12:34 +0530

    dto is

---
+++ GoldDTO1.java
@@ -0,0 +1,13 @@
+class GoldDTO1
+{
+public static void main(String a[])
+{
+GoldDTO gold=new GoldDTO();
+gold.setOwnerName("CHETHANA C A");
+gold.setShopName("CAC");
+gold.setTypeOfGold("24carot");
+gold.setWeight(10);
+gold.setCost(45000);
+System.out.println(gold.getOwnerName()+"\n"+gold.getShopName()+"\n"+gold.getTypeOfGold()+"\n"+gold.getWeight()+"\n"+gold.getCost());
+}
+}
